9ae9f0d2c93794ceb32be5a07e31e03423627a6f
irfanrangga <irfanrangga04@gmail.com>
2024-03-25 08:57:17 +0700

    Modif file aktivitas.go

--- week3/aktivitas.go
+++ week3/aktivitas.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func belajarMain(aktivitas string, belajar,main *int){
-	if aktivitas == "belajar"{
-		*belajar = *belajar + 1
-	} else if aktivitas == "main"{
-		*main = *main + 1
-	} 
+func belajarMain(aktivitas string, belajar, main *int) {
+	if aktivitas == "belajar" {
+		*belajar += 1
+	} else if aktivitas == "main" {
+		*main += 1
+	}
 }
 
-func main(){
+func main() {
 	var (
 		jumlahBelajar, jumlahMain int
-		aktivitas string
+		aktivitas                 string
 	)
-	
+
 	for aktivitas != "tidur" {
 		fmt.Scan(&aktivitas)
 		belajarMain(aktivitas, &jumlahBelajar, &jumlahMain)
