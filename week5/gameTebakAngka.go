545c8b7356d2a6a8f8a241b958b0d80ada2dde70
Irfan Rangga <irfanrangga04@gmail.com>
2024-03-29 18:40:34 +0700

    update file gameTebakAngka.go

--- week5/gameTebakAngka.go
+++ week5/gameTebakAngka.go
@@ -14,28 +14,30 @@ func game(m1, m2, m3 *int) {
 	var p1, p2, p3 bool
 	fmt.Scan(&t1, &t2, &t3)
 	for t1 != 0 && t2 != 0 && t3 != 0 {
-		fmt.Scan(&t1, &t2, &t3)
 		if t1%2 == 0 {
 			p1 = true
-		} else if t2%2 == 0 {
+		} else if t1%2 != 0 {
+			p1 = false
+		}
+		if t2%2 == 0 {
 			p2 = true
-		} else if t3%2 == 0 {
+		} else if t2%2 != 0 {
+			p2 = false
+		}
+		if t3%2 == 0 {
 			p3 = true
+		} else if t3%2 != 0 {
+			p3 = false
 		}
-		if p1 && p2 == false && p3 == false {
-			*m1 += 1
-		} else if p1 == false && p2 && p3 {
+		if p1 && p2 == false && p3 == false || p1 == false && p2 && p3 {
 			*m1 += 1
 		}
-		if p1 == false && p2 && p3 == false {
-			*m2 += 1
-		} else if p1 && p2 == false && p3 {
+		if p1 == false && p2 && p3 == false || p1 && p2 == false && p3 {
 			*m2 += 1
 		}
-		if p1 == false && p2 == false && p3 {
-			*m3 += 1
-		} else if p1 && p2 && p3 == false {
+		if p1 == false && p2 == false && p3 || p1 && p2 && p3 == false {
 			*m3 += 1
 		}
+		fmt.Scan(&t1, &t2, &t3)
 	}
 }
